ea5523ccf2e2af968af9f74564f95871f90957dd
tray190 <belltracy411@gmail.com>
2021-12-14 09:38:38 -0600

    class

---
+++ Classes Exercise/Classes Exercise/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Exercise
+{
+    public class Car
+    {
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+         }
+       
+        public  string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public Car()
+
+        {
+
+        }
+
+    }
+}
---
+++ Classes Exercise/Classes Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classes_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Car1 = new Car();
+            Car1.Make = "Tesla";
+            Car1.Model = "ModelX";
+            Car1.Year = 2019;
+
+            Console.WriteLine($"{Car1.Make}");
+            Console.WriteLine($"{Car1.Model}");
+            Console.WriteLine($"{Car1.Year}");
+                {
+
+                }
+            }
+        }
+    }
+}
